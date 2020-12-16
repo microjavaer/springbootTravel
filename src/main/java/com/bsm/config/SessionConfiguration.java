ebc75e66024ad32645961ed3ee5b99f21072c7b9
baoshumin <shumin.bao@molbase.com>
2020-12-16 11:03:49 +0800

    开启权限认证

--- src/main/java/com/bsm/config/SessionConfiguration.java
+++ src/main/java/com/bsm/config/SessionConfiguration.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//@Configuration
+@Configuration
 public class SessionConfiguration implements WebMvcConfigurer {
 
     @Override
